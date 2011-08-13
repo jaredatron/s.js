8dbc38febbf0da3c2a27041d9a9ce212f212cfd8
Jared Grippe <jared@deadlyicon.com>
2011-08-12 20:30:18 -0700

    added better .end API and added simple jQuery support

--- src/s.js
+++ src/s.js
@@ -9,40 +9,48 @@
   function S(value){
     return Selector(value);
   }
+  S.toSelector = S.toString = S.valueOf = function(){ return ''; }
+  S.end = S;
 
   function Selector(value, parent) {
-    if (value === undefined || EMTPY.test(value)) throw 'Selector cannot be empty';
+    // if (value === undefined || EMTPY.test(value)) throw 'Selector cannot be empty';
+    if (value === undefined) value = '';
     if (typeof value.toString === 'function') value = value.toString();
     validateSelector(value);
     value = strip(value);
     var selector = function(value){
-      return Selector(value, arguments.callee);
+      return arguments.length ? Selector(value, selector) : selector;
     };
     selector.value = value;
-    selector.end = parent;
-    selector.toSelector = selector.toString = selector.valueOf = toSelector;
+    selector.end = parent ? parent : S;
+    extend(selector, Selector.prototype);
     return selector;
   }
 
-  function toSelector() {
+  Selector.prototype.isSelector = true;
+  Selector.prototype.toSelector = function(){
     var
       self      = this,
       selectors = self.value.split(COMMA);
 
     validateSelector(self.value);
 
+    // TODO uniq selectors before returning
     return selectors.map(function(selector, parent_selectors){
       selector = strip(selector);
-      if (self.end){
-        if (selector.indexOf('&') === -1) selector = '& '+selector;
-        parent_selectors = self.end.toString().split(COMMA);
-        return parent_selectors.map(function(parent_selector){
-          return selector.replace(/&/g, parent_selector);
-        }).join(', ');
-      }
-      return selector;
+      if (self === self.end || self.end === S) return selector;
+      if (selector.indexOf('&') === -1) selector = '& '+selector;
+
+      parent_selectors = self.end.toString().split(COMMA);
+
+      return parent_selectors.map(function(parent_selector){
+        return selector.replace(/&/g, parent_selector);
+      }).join(', ');
+
     }).join(', ');
   }
+  Selector.prototype.toString = Selector.prototype.toSelector;
+  Selector.prototype.valueOf  = Selector.prototype.toSelector;
 
   global.S = S;
   global.Selector = Selector;
@@ -60,5 +68,10 @@
       .replace(COMMA, ', ')
     ;
   }
+
+  function extend(object, extension){
+    for (var p in extension) object[p] = extension[p];
+  }
+
 
 }(this, jQuery);
