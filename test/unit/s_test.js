0cade7265552a483c29b45365fd5ea5f1d9a9c6e
Jared Grippe <jared@deadlyicon.com>
2011-08-13 19:22:13 -0700

    split up tests into multiple tests

--- test/unit/s_test.js
+++ test/unit/s_test.js
@@ -2,16 +2,14 @@
 
   module("S");
 
-  test("S", function() {
-    var body = S('body');
-
-    // S
+  test("S is a string", function() {
     expect( S         ).toEqual('');
-    expect( S()       ).toEqual('')
-    expect( S('')     ).toEqual('')
+    expect( S()       ).toEqual('');
+    expect( S('')     ).toEqual('');
     expect( S('html') ).toEqual('html');
+  });
 
-    // end
+  test(".end", function() {
     expect( S                              ).toEqual('');
     expect( S.end                          ).toEqual('');
     expect( S('html').end                  ).toEqual('');
@@ -20,35 +18,39 @@
     expect( S('html').end()                ).toEqual('');
     expect( S('html')('body').end()        ).toEqual('html');
     expect( S('html')('body')('div').end() ).toEqual('html body');
+  });
 
-    // self returning
+  test("self returning", function() {
+    var body = S('body');
+    expect( S      ).toBe(S);
     expect( body() ).toBe(body);
+  });
 
-    // chaining
+  test("chaining", function() {
     expect( S('html body')                  ).toEqual('html body');
     expect( S('html body')('section')       ).toEqual('html body section');
     expect( S('html body')('section')('h1') ).toEqual('html body section h1');
+  });
 
-    // &
+  test("&", function() {
     expect( S('a')('&:visible')).toEqual('a:visible');
+  });
 
-    // ,
+  test(",", function() {
     expect( S('html')('div,span')        ).toEqual('html div, html span');
     expect( S('head,body')('div')        ).toEqual('head div, body div');
     expect( S('head,body')('div,span')   ).toEqual('head div, body div, head span, body span');
     expect( S('html')('div,span')('a,b') ).toEqual('html div a, html span a, html div b, html span b');
+  });
 
-    // white space
+  test("white space", function() {
     expect( S('  html  ')                      ).toEqual('html');
     expect( S('  html  ')('  head  ,  body  ') ).toEqual('html head, html body');
     expect(function(){ S('a,b,').toString(); }).toThrow('Selectors can\'t end in commas. "a,b,"');
+  });
 
-
-    // S takes an S object
+  test("S takes an S", function() {
     expect( S(S('html'))).toEqual('html');
-
-
-
   });
 
 
